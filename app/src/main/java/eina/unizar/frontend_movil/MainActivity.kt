e93c96148f5cda26697ab1d6f97c888d73c9019a
874802 <874802@unizar.es>
2025-04-26 16:30:05 +0200

    Implementación de música

--- app/src/main/java/eina/unizar/frontend_movil/MainActivity.kt
+++ app/src/main/java/eina/unizar/frontend_movil/MainActivity.kt
@@ -1,20 +1,48 @@
 package eina.unizar.frontend_movil
 
-
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import eina.unizar.frontend_movil.ui.navigation.AppNavigation
-
+import eina.unizar.frontend_movil.ui.utils.MusicManager
 
 class MainActivity : ComponentActivity() {
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp()
         }
+        iniciarMusicaDeFondo()
+    }
+
+    private fun iniciarMusicaDeFondo() {
+        MusicManager.mediaPlayer = MediaPlayer.create(this, R.raw.music1)
+        MusicManager.mediaPlayer?.apply {
+            isLooping = true
+            setVolume(MusicManager.volume.value, MusicManager.volume.value)
+            start()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer?.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mediaPlayer?.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
 
--- app/src/main/java/eina/unizar/frontend_movil/ui/utils/MusicManager.kt
+++ app/src/main/java/eina/unizar/frontend_movil/ui/utils/MusicManager.kt
@@ -1,2 +1,9 @@
 package eina.unizar.frontend_movil.ui.utils
 
+import android.media.MediaPlayer
+import androidx.compose.runtime.mutableStateOf
+
+object MusicManager {
+    var volume = mutableStateOf(0.38f)
+    var mediaPlayer: MediaPlayer? = null
+}
