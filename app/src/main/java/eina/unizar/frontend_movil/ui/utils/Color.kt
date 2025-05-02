aca49be753e4880462e6dd07cf7bab8d93a857f5
dborrel <871643@unizar.es>
2025-05-02 18:11:21 +0200

    Pantallas de integración de la tienda

---
+++ app/src/main/java/eina/unizar/frontend_movil/ui/utils/Color.kt
@@ -0,0 +1,12 @@
+package eina.unizar.frontend_movil.ui.utils
+
+import androidx.compose.ui.graphics.Color
+
+// Colores existentes (suponiendo que ya están definidos)
+val PurpleBackground = Color(0xFF20002c) // Ejemplo de color púrpura oscuro
+val TextWhite = Color(0xFFF8F8F8)
+val CardGray = Color(0xFF424242)
+val SuccessGreen = Color(0xFF4CAF50)
+
+// Nuevo color dorado
+val GoldColor = Color(0xFFFFD700)
